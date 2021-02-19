6cf5a5db4c2932c631a2ea8f657fd369db881170
Abiud Benitez <79248835+WenasNoshies@users.noreply.github.com>
2021-02-19 01:19:21 -0600

    Add files via upload

---
+++ Suma.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+main(){
+	int A, B, C;
+	printf("Escribe el primer valor: ");
+	scanf("%d", &A);
+	printf("Escribe el segundo valor: ");
+	scanf("%d", &B);
+	C = A+B;
+	printf("El valor de la suma es de: %d ", C);
+	getch();
+}
