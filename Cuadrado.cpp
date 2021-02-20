9177d27004975f1185fc1430869f909b0afa8697
Abiud Benitez <abiudmisael10@hotmail.com>
2021-02-19 23:58:24 -0600

    Hector es feo

---
+++ Cuadrado.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+	int Lad, Higth, Width, Num1, Num2, Num0;
+	printf("Programa para saber cuantas veces cabe un cuadrado dentro de un rectangulo segun sus medidas\n\n");
+	printf("Ingrese el valor del lado del cuadrado: ");
+	scanf("%d", &Lad);
+	printf("\nIngrese el valor de la altura del rectangulo: ");
+	scanf("%d", &Higth);
+	printf("\nIngrese el valor del ancho del rectangulo: ");
+	scanf("%d", &Width);
+	Num1 = Higth / Lad;
+	Num2 = Width / Lad;
+	Num0 = Num1 * Num2;
+	printf("\nEl numero de veces que cabe el cuadrado en el rectangulo es de: %d", Num0);
+	getch();
+}
---
+++ Temperatura.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+	printf("Programa para convertir la temperatura en grados celsius a farenheit y kelvin");
+	float Cel, Kel, Far;
+	
+}
