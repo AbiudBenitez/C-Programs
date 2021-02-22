9fcd378f191af0004dd61487948d618851c56140
Abiud Benitez <abiudmisael10@hotmail.com>
2021-02-21 20:39:01 -0600

    El programa de temperatura se completo

--- Temperatura.cpp
+++ Temperatura.cpp
@@ -2,7 +2,13 @@
 #include <conio.h>
 
 main(){
-	printf("Programa para convertir la temperatura en grados celsius a farenheit y kelvin");
+	printf("Programa para convertir la temperatura en grados celsius a farenheit y kelvin\n");
 	float Cel, Kel, Far;
-	
+	printf("\nIngrese el valor de la temperatura en grados centigrados: ");
+	scanf("%f", &Cel);
+	Kel = 273.15 + Cel;
+	printf("\nLa temperatura en Kelvin es de %.1f ", Kel);
+	Far = (1.8 * Cel) + 32;
+	printf("\nLa temperatura en grados farenheit es de %.1f", Far);
+	getch();
 }
