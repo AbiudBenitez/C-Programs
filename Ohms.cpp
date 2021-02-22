20ef318323ca88104286fc49a547bee54b932f4b
Abiud Benitez <abiudmisael10@hotmail.com>
2021-02-21 19:00:37 -0600

    asies

---
+++ Ohms.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+main(){
+	printf("Programa para calcular el voltaje con la ley de Ohms\n\n");
+	int I, R, V;
+	printf("Inserte el valor de la corriente electrica: ");
+	scanf("%d", &I);
+	printf("\nInserte el valor de la resistencia: ");
+	scanf("%d", &R);
+	V = I * R ;
+	printf("\n El valor del voltaje es de %d volts", V);
+}
