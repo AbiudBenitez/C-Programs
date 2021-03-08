31e9a2b67a39019651e52c555ac5f7fa8c74db87
Abiud Benitez <abiudmisael10@hotmail.com>
2021-03-08 00:36:56 -0600

    Algunos cambitos nomas

---
+++ 1.4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+	char Pro, I;
+	float N1, N2, R;
+	printf("CALCULADORA\n\n");
+	scanf("%f %c %f", &N1, &Pro, &N2);
+	getche(); 
+	printf("=");
+	
+	getch();
+}
--- 1.5.cpp
+++ 1.5.cpp
@@ -2,24 +2,29 @@
 #include <conio.h>
 
 main(){
-	printf("Programa que dado un grupo de 3 alumnos con sus calificaciones correspondientes, indica cuantos aprobaron y cuantos reporbaron ademas de cual es el porcentaje de aprobacion\n");
+	printf("Programa que dado un grupo de 3 alumnos con sus calificaciones correspondientes, indica cuantos aprobaron y cuantos reprobaron ademas de cual es el porcentaje de aprobacion\n\n");
 	float A, R, Por;
-	int Calif[2], c;
+	int Calif[3], c, Mat[3], i;
 	for(c = 1; c <= 3; c++){
-		printf("Introduce una calificacion: ");
+		printf("Introduce una matricula: ");
+		scanf("%d", &Mat[c]);
+		printf("Introduce su calificacion: ");
 		scanf("%d", &Calif[c]);
 		if(Calif[c] >= 70){
 			if(Calif[c] <= 100){
 				A = A + 1;
 			}else{
-				printf("La maxima calificacion es de 100");
+				printf("ERROR: La maxima calificacion es de 100\n");
 				Calif[c] = 0;
 			}
 		}else{
 			R = R + 1;
 		}
 	}
+	for(i = 1; i<=3; i++){
+		printf("\nEl alumno con la matricula %d saco: %d", Mat[i], Calif[i]);
+	}
 	Por = (A * 100) / 3;
-	printf("\nLos alumnos que aprobaron fueron de %.0f y los que reprobaron de %.0f con un procentaje de aprobacion de %.0f porciento", A, R, Por);
+	printf("\n\nEl porcentaje de aprobacion es del: %.0f porciento", Por);
 	getch();
 }
