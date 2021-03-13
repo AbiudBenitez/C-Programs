d97fde6b3e62655a83c6124b91135055f8489cdc
Abiud Benitez <abiudmisael10@hotmail.com>
2021-03-13 03:00:06 -0600

    xd ta todo mal

--- 1.7.cpp
+++ 1.7.cpp
@@ -2,11 +2,21 @@
 #include <conio.h>
 
 main(){
-	printf("Programa que imprime unicamente el numero indicado de terminos de la siguiente serie");
-	int i, c;
+	printf("Programa que imprime unicamente el numero indicado de terminos de la siguiente serie\n\n");
+	int i, c, s;
+	s = 2;
 	printf("Indique el numero de caracteres que quiere que imprima la serie: ");
-	scanf("%d", c);
-	for(i = 2; i < c; i++){
-		
+	scanf("%d", &c);
+	printf("%d", s);
+	for(i = 1; i < c; i++){
+		s = s * 2;
+		printf("%d", s);
+		if(c % 2 == 0){
+			for(i = 1; i < c; i++){
+				s = s - 1;
+				printf("%d", s);
+			}
+		}
 	}
+	getch();
 }
