e4c96e1f47c3fa5e55722b8cf7e6bbdf064b6a91
Abiud Benitez <abiudmisael10@hotmail.com>
2021-03-13 03:10:45 -0600

    Ya lo arregle UwU

--- 1.7.cpp
+++ 1.7.cpp
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <conio.h>
 
-main(){
+main()
+{
 	printf("Programa que imprime unicamente el numero indicado de terminos de la siguiente serie\n\n");
 	int i, c, s;
 	s = 2;
 	printf("Indique el numero de caracteres que quiere que imprima la serie: ");
 	scanf("%d", &c);
-	printf("%d", s);
-	for(i = 1; i < c; i++){
-		s = s * 2;
-		printf("%d", s);
-		if(c % 2 == 0){
-			for(i = 1; i < c; i++){
-				s = s - 1;
-				printf("%d", s);
-			}
+	for(i = 1; i <= c; i++)
+	{
+		if(i % 2 == 0){
+			s = s * 2;
+		}else if(i == 1){
+		}else{
+			s = s - 1;
 		}
+		printf("%d ", s);
 	}
 	getch();
 }
