587c54728c9c50dac010982f6bec3998319d8e25
Abiud Benitez <abiudmisael10@hotmail.com>
2021-03-13 02:06:56 -0600

    avance

---
+++ 1.7.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+main(){
+	printf("Programa que imprime unicamente el numero indicado de terminos de la siguiente serie");
+	int i, c;
+	printf("Indique el numero de caracteres que quiere que imprima la serie: ");
+	scanf("%d", c);
+	for(i = 2; i < c; i++){
+		
+	}
+}
