5c8b57fbafecbdc585b52952f6236282e7fb8082
Abiud Benitez <abiudmisael10@hotmail.com>
2021-03-13 03:54:18 -0600

    Ni le avance xd

--- 1.7.5.c
+++ 1.7.5.c
@@ -3,5 +3,17 @@
 #include <time.h>
 
 main(){
-	
+	printf("Reloj");
+	int Ho, h, Mi, m, Se, s;
+	printf("Inserte las horas: ");
+	scanf("%d", h);
+	printf("Inserte los minutos: ");
+	scanf("%d", m);
+	printf("Inserte los segundos: ");
+	scanf("%d", s);
+	for(Se = 0; Mi < m + 1 && Se < s + 1 && Ho < h + 1; Se++)
+	{
+		
+	}
+	getch();
 }
