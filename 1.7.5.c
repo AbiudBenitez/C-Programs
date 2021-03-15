9b5c34556f54ded21383fb89e464ee21ea269802
Abiud Benitez <abiudmisael10@hotmail.com>
2021-03-15 01:09:04 -0600

    Aun no logro que funcione ; -;

--- 1.7.5.c
+++ 1.7.5.c
@@ -2,18 +2,32 @@
 #include <conio.h>
 #include <time.h>
 
-main(){
+main()
+{
 	printf("Reloj\n");
 	int Ho, h, Mi, m, Se, s;
+	Se = 0;
+	Ho = 0;
+	Mi = 0;
 	printf("Inserte las horas: ");
-	scanf("%d", h);
+	scanf("%d", &h);
 	printf("Inserte los minutos: ");
-	scanf("%d", m);
+	scanf("%d", &m);
 	printf("Inserte los segundos: ");
-	scanf("%d", s);
-	for(Se = 0; Mi < m + 1 && Se < s + 1 && Ho < h + 1; Se++)
+	scanf("%d", &s);
+	for(Se = 1; Se < s + 1 && Mi < m && Ho < h + 1; Se++)
 	{
-		
+		if(Se == 60)
+		{
+			Mi = Mi + 1;
+			Se = 0;
+		}
+		if(Mi == 60)
+		{
+			Ho = Ho + 1;
+			Mi = 0;
+		}
+		printf("%d:%d:%d\n", Ho, Mi, Se);
 	}
 	getch();
 }
