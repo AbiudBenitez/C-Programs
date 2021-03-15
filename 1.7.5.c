394169ba72a28cfc066df6cca20307db1dbd483c
Abiud Benitez <abiudmisael10@hotmail.com>
2021-03-15 01:13:45 -0600

    creo que no debi de hacer esto xd

--- 1.7.5.c
+++ 1.7.5.c
@@ -16,18 +16,6 @@ main()
 	printf("Inserte los segundos: ");
 	scanf("%d", &s);
 	for(Se = 1; Se < s + 1 && Mi < m && Ho < h + 1; Se++)
-	{
-		if(Se == 60)
-		{
-			Mi = Mi + 1;
-			Se = 0;
-		}
-		if(Mi == 60)
-		{
-			Ho = Ho + 1;
-			Mi = 0;
-		}
-		printf("%d:%d:%d\n", Ho, Mi, Se);
-	}
+
 	getch();
 }
