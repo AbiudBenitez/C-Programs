e83a7614c29e0fadd36b9ccd17ac78a6edc824d0
Abiud Benitez <abiudmisael10@hotmail.com>
2021-03-15 20:16:37 -0600

    Aun no hago nada

--- 1.7.5.c
+++ 1.7.5.c
@@ -15,19 +15,25 @@ main()
 	scanf("%d", &m);
 	printf("Inserte los segundos: ");
 	scanf("%d", &s);
-	while((Se < s + 1) && (Mi < m + 1) && (Ho < h + 1))
+	for(;Ho < h;)
 	{
-		if(Se == 60)
+		printf("%d:%d:%d\n", Ho, Mi, Se);
+		for(;Mi < m; Se++)
 		{
-			Mi = Mi + 1;
-			Se = 0;
+			printf("%d:%d:%d\n", Ho, Mi, Se);
+			if(Se == 60)
+			{
+				Mi = Mi + 1;
+				Se = 0;
+				printf("%d:%d:%d\n", Ho, Mi, Se);
+			}
 			if(Mi == 60)
 			{
 				Ho = Ho + 1;
+				Mi = 0;
+				printf("%d:%d:%d\n", Ho, Mi, Se);
 			}
 		}
-		printf("%d:%d:%d\n", Ho, Mi, Se);
-		Se++;
 	}
 	getch();
 }
