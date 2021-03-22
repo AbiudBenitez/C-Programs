1dd94dee0a902f715b7393d12a40ea6520a3407e
Abiud Benitez <abiudmisael10@hotmail.com>
2021-03-21 18:27:42 -0600

    No le entiendo xd

--- Ejemplos/Lista.cpp
+++ Ejemplos/Lista.cpp
@@ -13,9 +13,39 @@ Libro *ListaLibro(Libro *Lista)
 	return Lista;
 }
 
+Libro *agregarLibro(Libro *Lista, char *nombre)
+{
+	Libro *nuevoLibro, *aux;
+	nuevoLibro = (Libro*)malloc(sizeof(Libro));
+	nuevoLibro -> nombre = nombre;
+	nuevoLibro -> sig = NULL;
+	if(Lista == NULL)
+	{
+		Lista = nuevoLibro;
+	}
+	else
+	{
+		aux = Lista;
+		while(aux->sig != NULL)
+		{
+			aux = aux->sig;
+			
+		}
+		aux->sig = nuevoLibro;
+	}
+	return Lista;
+}
+
 int main()
 {
 	Libro *Lista = ListaLibro(Lista);
-	printf("%p", Lista);
+	Lista = agregarLibro(Lista, "Primera Lista");
+	Lista = agregarLibro(Lista, "Segunda Lista");
+	while(Lista != NULL)
+	{
+		printf("%s\n", Lista->nombre);
+		Lista = Lista->sig;
+	}
+	
 	return 0;
 }
