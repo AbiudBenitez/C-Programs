7b66c8596abb43f2087fbad391e3fcb1f6a58d46
WenasNoshies <abiudmisael10@hotmail.com>
2021-05-27 18:42:12 -0500

    Ya no esperes

--- Ejemplos/PE/EO/five.c
+++ Ejemplos/PE/EO/five.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
-void main()
+int main()
 {
     double i, Sum;
     Sum = 0;
-    for(i=0;i<10;i++)
+    for(i=1;i<11;i++)
     {
-        Sum = Sum + pow(i, i);
+        Sum = Sum + pow(i, 2);
     }
     printf("La suma de los cuadrados de los primeros 10 numeros enteros es de: %f", Sum);
+    return 0;
 }
